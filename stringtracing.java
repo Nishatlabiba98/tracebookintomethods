735f05c5df03eb7cb521293acaffb2779ebc29d8
nishat <nishatlabiba98@gmail.com>
2026-05-27 08:00:10 -0400

    stringjava so I dont forget Java

--- stringtracing.java
+++ stringtracing.java
@@ -1,3 +1,12 @@
-public class stringtracing {
-    
+public static String reverseWords(String s) {
+    String[] words = s.split(" ");
+    int left = 0, right = words.length - 1;
+    while (left < right) {
+        String temp = words[left];
+        words[left] = words[right];
+        words[right] = temp;
+        left++;
+        right--;
+    }
+    return String.join(" ", words);
 }
