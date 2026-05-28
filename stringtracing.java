2125c6bdff6efcd3958422c4675d5c66142435c0
nishat <nishatlabiba98@gmail.com>
2026-05-28 07:02:07 -0400

    a wb a day keeps u in what?

--- stringtracing.java
+++ stringtracing.java
@@ -1,3 +1,4 @@
+//1 .  
 public static String reverseWords(String s) {
     String[] words = s.split(" ");
     int left = 0, right = words.length - 1;
@@ -10,3 +11,14 @@ public static String reverseWords(String s) {
     }
     return String.join(" ", words);
 }
+//2. return the nuber of times a target character appears in a string.
+public static int countChar(String s, char target) {
+    int count = 0;
+    for ( char c : s.toCharArray()) {
+        if (c == target) {
+            count++;
+        }
+    }
+    return count;
+}
+//access modifier, return type, method name, parameters and method body. method signature is static- no other object needed to call it- takes a string and returns it. int count is initialized at 0 . toCharArray breaks the strings into an array of characters. the for each loop visits each character and each time it matches the target, the counter goes up by 1. loop is done, string and target is passed by the user.
