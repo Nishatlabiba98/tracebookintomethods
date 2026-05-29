e5b65a47bd0acc4eb61a164f1320000e5e45e620
nishat <nishatlabiba98@gmail.com>
2026-05-29 06:53:53 -0400

    method isPalindrome in from friday wb's

--- stringtracing.java
+++ stringtracing.java
@@ -22,3 +22,20 @@ public static int countChar(String s, char target) {
     return count;
 }
 //access modifier, return type, method name, parameters and method body. method signature is static- no other object needed to call it- takes a string and returns it. int count is initialized at 0 . toCharArray breaks the strings into an array of characters. the for each loop visits each character and each time it matches the target, the counter goes up by 1. loop is done, string and target is passed by the user.
+
+//3. write a method that determines if the passed string is a palindrome or not
+
+public boolean isPalindrome(String s) { // access modifier, return type, method name and passed parameter. 
+    int left = 0;
+    int right= s.length() - 1;
+    while (left < right) {
+        if (s.charAt(left) != s.charAt(right)) {
+            return false;
+        }
+        left++;
+        right--;
+        }
+        return true;
+    }
+
+
