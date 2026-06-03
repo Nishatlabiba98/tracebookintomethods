3a06623906663cc3f2c01399fb12690a096f73b4
nishat <nishatlabiba98@gmail.com>
2026-06-03 07:00:06 -0400

    nonrepeatingchar

--- stringtracing.java
+++ stringtracing.java
@@ -51,3 +51,19 @@ public boolean isAnagram(String s, String t) {
     return Arrays.equals(sArray, tArray);
 }
 
+5.public char firstNonRepeating(String s) {
+    HashMap<Character, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i);
+        map.put(c, map.getOrDefault(c, 0) + 1);
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+        if (map.get(s.charAt(i)) == 1) {
+            return s.charAt(i);
+        }
+    }
+
+    return ' '; // no non-repeating character found
+}
